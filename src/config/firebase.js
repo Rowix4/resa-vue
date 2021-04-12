7b90cf06a620efb649ffc418f2e890644cf2a425
Léo GIGANT <lg@bsoft.fr>
2021-04-12 21:14:00 +0200

    Added firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyDshsynqp_dl43t0D28UB8wX3roV_ptVVU",
+  authDomain: "resavue-ad147.firebaseapp.com",
+  databaseURL: "https://resavue-ad147.firebaseio.com",
+  projectId: "resavue-ad147",
+  storageBucket: "resavue-ad147.appspot.com",
+  messagingSenderId: "421694191713",
+};
+
+export default config;
